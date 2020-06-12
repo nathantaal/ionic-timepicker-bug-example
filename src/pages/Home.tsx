bfaa02862daf6cd31811fa35d829582665bc1272
Nathan <me@nathantaal.nl>
2020-06-12 08:21:28 +0200

    Add actual example of bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonContent, IonDatetime, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import React, {useState} from 'react';
 import './Home.css';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+export default function Home() {
+
+    const [selectedDate, setSelectedDate] = useState<string>('2012-12-15T13:47:20.789');
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Timepicker bug (Ion-Datetime) example</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonItem>
+                    <IonLabel>HH:mm</IonLabel>
+                    <IonDatetime displayFormat="HH:mm" value={selectedDate}
+                                 onIonChange={e => setSelectedDate(e.detail.value!)}/>
+                </IonItem>
+            </IonContent>
+        </IonPage>
+    );
 };
 
-export default Home;
